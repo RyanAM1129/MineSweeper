6ef461ed87378e17f82e664ed5d3b30efb3588b9
RyanAM1129 <RyanAMacLeod@hotmail.com>
2022-04-07 12:58:09 -0700

    commit three

---
+++ src/Controller/Converter.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class Converter {
+    public int[] convertFieldSize(String theFieldSize) {
+        
+    }
+}
---
+++ src/View/Main.java
@@ -0,0 +1,11 @@
+package View;
+
+import Model.MineField;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+    }
+}
